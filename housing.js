b3dce8fbdb0c24ce45a35cbc790bc7257ecfb6cd
David Bell <thisisdavidbell@hotmail.com>
2017-03-03 14:55:28 +0000

    working APIs

--- housing.js
+++ housing.js
@@ -1,13 +1,129 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var housing = express();
 
-// base path for housing APIs.
-housing.get('/', function(req, res) {
-  res.sendFile(__dirname + '/housing.html');
-});
+//housing.use(bodyParser.json());
+var jsonParser = bodyParser.json()
 
 // the APIs
+
+//////////////////////////////////////////////////////////////////////////////////
+// API: lookup house id
+
+var ids = [ 12345, 2468, 98765, 54321, 11111, 44444, 77777, 99999, 30003, 64201];
+
+housing.get('/houseid', function(req, res) {
+
+    res.setHeader("Content-Type", "application/json");
+
+    var housenumber = req.query.housenumber;
+    var postcode = req.query.postcode;
+
+    if(postcode != null && postcode != ""
+        && housenumber != null && housenumber != "") {
+        // Return Successful API invoke
+        var digit = housenumber.charAt(0);
+        if (digit.match(/^[0-9]/)){
+  	      res.json(ids[digit]);
+     	  } else {
+  	      res.status(400).json( {error: 'house number starts with non-numeric character.'});
+  	    }
+        //res.json({housing: ' req.query.postcode = ' + req.query.postcode});
+    } else {
+  	  res.status(400).json( {error: 'You must provide a query parameter postcode, set to a postcode'});
+    } // end check query param postcode
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+// API: house owner
+
+var owners = [
+  { id: '12345', owner : "David Bell", mortgageprovider: "Bank A"},
+  { id: '2468', owner : "David Seager", mortgageprovider: "Local Bank"},
+  { id: '98765', owner : "Ben March", mortgageprovider: "Best Building Society"},
+  { id: '54321', owner : "David Martin", mortgageprovider: "Online Banking Co"},
+  { id: '11111', owner : "John Smith", mortgageprovider: "Bank Z"},
+  { id: '44444', owner : "Matthew Saveal", mortgageprovider: "Another Bank"},
+  { id: '77777', owner : "Christine Creasey", mortgageprovider: "Mortgage Co"},
+  { id: '99999', owner : "Jessica Jones", mortgageprovider: "Expensive Mortgages Are Us"},
+  { id: '30003', owner : "Eddie Hutson", mortgageprovider: "Bargain Mortgages Company"},
+  { id: '64201', owner : "Jane Doe", mortgageprovider: "Global Bank"}
+];
+
+function findowner(theid) {
+  for (var i = 0; i <owners.length; i++) {
+    if (owners[i].id == theid) {
+      return owners[i];
+    }
+
+  } // end for
+
+  // no match
+  return "Error: no house with id: " + theid;
+}
+
+function idexists(theid) {
+  for (var i = 0; i <owners.length; i++) {
+    if (owners[i].id == theid) {
+      return i;
+    }
+  }
+  // no match
+  return -1;
+}
+
+
+housing.get('/houseowner', function(req, res) {
+
+    res.setHeader("Content-Type", "application/json");
+
+    var houseid = req.query.houseid;
+
+    if(houseid != null && houseid != ""){
+        res.json(findowner(houseid));
+    } else {
+    	res.status(400).json( {error: 'You must provide a query parameter houseid, set to a valid house record id'});
+    } // end check query param postcode
+
+});
+
+housing.put('/houseowner', jsonParser, function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+
+    var houseid = req.query.houseid;
+    if (houseid == null || houseid == "") {
+      res.status(400).json( {error: 'You must provide a message body containing owner and mortgageprovider fields'});
+    }
+
+    if(req.body != null && req.body != ""
+      && req.body.owner != null && req.body.owner != ""
+      && req.body.mortgageprovider != null && req.body.mortgageprovider != "") {
+      var newowner = req.body.owner;
+      var newmortgageprovider = req.body.mortgageprovider;
+    } else {
+      res.status(400).json( {error: 'You must provide a message body containing owner and mortgageprovider fields'});
+    }
+
+    var arrayindex = idexists(houseid);
+    if (arrayindex == -1) {
+      console.log("no id")
+       res.status(400).json( {error: ' no house with id:'  + theid});
+    } else {
+       owners[arrayindex].owner = newowner;
+       owners[arrayindex].mortgageprovider = newmortgageprovider;
+       console.log("changed");
+       res.sendStatus(202);
+    }
+  //  res.json(findowner(houseid));
+
+
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////
+// API: postcode lookup
 
 var locations = [
   { long : '1.39870', lat : "51.02655", building: "Bank A", street: "Hursley Park Road", area: "Hursley", town: "Winchester", county: "Hampshire", postcode: "SO21 2JN"},
@@ -17,19 +133,13 @@ var locations = [
   { long : '-1.156350', lat : "52.954911", building: "Bank D", street: "City Gate West", area: "Tollhouse Hill", town: "Nottingham", county: "Nottinghamshire", postcode: "NG1 5FN"},
 ];
 
-// Locate ATM
 housing.get('/postcodelookup', function(req, res) {
 
   res.setHeader("Content-Type", "application/json");
 
-
-  // print out header drbheader
-  var drbh = req.header('drbheader');
-  console.log("drbheader: " + drbh);
-
   // check headers //
   var CTHeader = req.header('Content-Type');
-  console.log("Content-Type: " + CTHeader);
+
   var AcceptHeader = req.header('Accept');
   //console.log("Accept: " + AcceptHeader);
   // use Accept header to determine return type
@@ -49,16 +159,16 @@ housing.get('/postcodelookup', function(req, res) {
   } // end check Accept header if
 
   // check we have the required query parameter postcode
-  var userloc = req.query.postcode;
-  if(userloc != null) {
+  var postcode = req.query.postcode;
+  if(postcode != null && postcode != "") {
       // Return Successful API invoke
-      if (userloc.match(/^so|SO/)){
+      if (postcode.match(/^so|SO/)){
 	    res.json(locations[0]);
-	  } else if (userloc.match(/^m|M/)){
+	  } else if (postcode.match(/^m|M/)){
 	    res.json(locations[1]);
-	  } else if (userloc.match(/^se|SE/)){
+	  } else if (postcode.match(/^se|SE/)){
 	    res.json(locations[2]);
-    } else if (userloc.match(/^pl|PL/)){
+    } else if (postcode.match(/^pl|PL/)){
 	    res.json(locations[3]);
 	  } else {
 	    res.json(locations[4]);
@@ -67,7 +177,7 @@ housing.get('/postcodelookup', function(req, res) {
 	  }
       //res.json({housing: ' req.query.postcode = ' + req.query.postcode});
   } else {
-	res.status(400).json( {error: 'You must provide a query parameter postcode, set to a postcode or town'});
+	res.status(400).json( {error: 'You must provide a query parameter postcode, set to a postcode'});
   } // end check query param postcode
 
   //res.sendFile(__dirname + '/housing.html');
